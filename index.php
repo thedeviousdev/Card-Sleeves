5bd13bbe72b142fa4c49d6d79b008107a1518ab6
thedeviousdev <blouish@hotmail.com>
2017-12-22 21:18:06 +1100

    ♥

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$row = 1;
+if (($handle = fopen("data/Sleeves.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        $row++;
+        for ($c=0; $c < $num; $c++) {
+            echo $data[$c] . "<br />\n";
+        }
+    }
+    fclose($handle);
+}
+?>
