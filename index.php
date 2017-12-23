eb811833601864d29060e981b6f30b357903a964
thedeviousdev <blouish@hotmail.com>
2017-12-23 15:01:12 +1100

    Adding games to DB & data

--- index.php
+++ index.php
@@ -1,13 +1,49 @@
 <?php
-$row = 1;
-if (($handle = fopen("data/Sleeves.csv", "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        $num = count($data);
-        $row++;
-        for ($c=0; $c < $num; $c++) {
-            echo $data[$c] . "<br />\n";
-        }
-    }
-    fclose($handle);
+$file = 'data/game-list.sqlite';
+
+if (!file_exists($file)) {
+	try {
+	  $db = new PDO('sqlite:data/game-list.sqlite');
+	  $db->exec("CREATE TABLE Game (Id INTEGER PRIMARY KEY, Name TEXT, Edition TEXT, CardNumber TEXT, Width INTEGER, Height INTEGER)");    
+		if (($handle = fopen("data/Sleeves.csv", "r")) !== FALSE) {
+		    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+	        $num = count($data);
+
+	        $name = $data[0];
+	        $edition = NULL;
+	        $card_number = $data[1];
+	        $width = $data[2];
+	        $height = $data[3];
+
+			    $db->exec("INSERT INTO Game (Name, Edition, CardNumber, Width, Height) VALUES ('" . $name . "', '" . $edition . "', '" . $card_number . "', '" . $width . "', '" . $height . "');");
+
+		    }
+		    fclose($handle);
+		}
+
+	  $db = NULL;
+	}
+	catch(PDOException $e)
+	{
+	  print 'Exception : '.$e->getMessage();
+	}
 }
+else {
+	  $db = new PDO('sqlite:data/game-list.sqlite');
+
+	  print "<table border=1>";
+	  print "<tr><td>Id</td><td>Name</td><td>Edition</td><td>CardNumber</td><td>Width</td><td>Height</td></tr>";
+	  $result = $db->query('SELECT * FROM Game');
+
+	  foreach($result as $row) {
+	    print "<tr><td>".$row['Id']."</td>";
+	    print "<td>".$row['Name']."</td>";
+	    print "<td>".$row['Edition']."</td>";
+	    print "<td>".$row['CardNumber']."</td>";
+	    print "<td>".$row['Width']."</td>";
+	    print "<td>".$row['Height']."</td></tr>";
+	  }
+	  print "</table>";
+}
+
 ?>
