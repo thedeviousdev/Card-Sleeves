3ea6f9f64096f48051412b581168ae7240b39622
thedeviousdev <blouish@hotmail.com>
2017-12-29 00:21:21 +1100

    Reworked search layout

---
+++ game_search.php
@@ -0,0 +1,38 @@
+<?php 
+if(isset($_GET['game'])) {
+	
+	$game_ID = $_GET['game'];
+	game_search($game_ID);
+
+}
+
+function game_search($g){
+
+	try {
+		$db = new PDO('sqlite:data/game-list_test.sqlite');
+
+	  $result = $db->query("SELECT * FROM Game WHERE Name LIKE '%" . $g . "%'");
+
+	  foreach($result as $row) {
+  	?>
+  	<div class="<?php echo $row['Id']; ?>">
+	  	<span data-game_id="<?php echo $row['Id']; ?>" class="btn_add"><i class="fas fa-plus-circle"></i></span>
+  		<div class="wrapper_img">
+		  	<img src="img/<?php echo $row['Image']; ?>" />
+	  	</div>
+	  	<div class="wrapper_text">
+		  	<h2><?php echo $row['Name']; ?></h2>
+		  	<sub><?php echo $row['Edition']; ?></sub>
+
+		  	<p><a href="<?php echo $row['URL']; ?>">BoardGameGeek</a></p>
+	  	</div>
+  	</div>
+	  <?php
+	  }
+	}
+	catch(PDOException $e) 	{
+	  print 'Exception : '. $e->getMessage();
+	}
+
+}
+?>
