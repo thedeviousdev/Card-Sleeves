3474b0fe83829faaa51e207a35d23643bf7d2cbf
thedeviousdev <blouish@hotmail.com>
2017-12-29 13:34:43 +1100

    Functions to add total of sleeves

---
+++ game_total.php
@@ -0,0 +1,100 @@
+<?php 
+include_once('game.php');
+include_once('card.php');
+include_once('sleeve.php');
+include_once('game_add.php');
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+if(isset($_GET['total'])) {
+	get_total_sleeves();
+}
+
+function set_sleeve($company_ID) {
+
+	$db = new PDO('sqlite:data/game-list_test.sqlite');
+  $result = $db->query("SELECT * FROM Sleeve WHERE CompanyID = '" . $company_ID . "'");
+
+  $sleeve_arr = array();
+
+  foreach($result as $card) {
+  	$sleeve_arr[] = new sleeve($card['SleeveName'], $card['Width'], $card['Height']);
+  }
+  return $sleeve_arr;
+}
+
+function get_sleeve_name($card) {
+
+	$card_width = $card->get_width();
+	$card_height = $card->get_height();
+
+	$sleeve_arr = set_sleeve('1');
+
+	$best_sleeve_name = NULL;
+	$best_difference = -1;
+	$temp_difference = -1;
+
+	foreach($sleeve_arr as $sleeve) {
+
+		$sleeve_name = $sleeve->get_name();
+		$sleeve_width = $sleeve->get_width();
+		$sleeve_height = $sleeve->get_height();
+
+		if($card_width <= $sleeve_width && $card_height <= $sleeve_height) {
+			$temp_difference = ($sleeve_width - $card_width) + ($sleeve_height - $card_height);
+			$temp_difference = (int)$temp_difference;
+		}
+
+		if($temp_difference == 0) {
+			$best_sleeve_name = $sleeve_name;
+			$best_difference = $temp_difference;
+			break;
+
+		}
+		if($best_difference == -1 || $temp_difference < $best_difference) {
+			$best_sleeve_name = $sleeve_name;
+			$best_difference = $temp_difference;
+		}
+
+	}
+
+	return $best_sleeve_name;	
+}
+
+function get_total_sleeves() {
+	if(isset($_SESSION['add_games'])) {
+		$game_arr = $_SESSION['add_games'];
+		$sleeve_arr = array();
+
+		foreach($game_arr as $key => $game) {
+
+			$card_arr = $game->get_cards();
+
+			foreach($card_arr as $key => $card) {
+				$sleeve_name = get_sleeve_name($card);
+				$sleeve_number = $card->get_nb_cards();
+
+				if (array_key_exists($sleeve_name, $sleeve_arr)) {
+					$sleeve_arr[$sleeve_name] += $sleeve_number;
+				}
+				else {
+					$sleeve_arr[$sleeve_name] = $sleeve_number;
+				}
+			}
+		}
+
+		foreach($sleeve_arr as $key => $sleeve) {
+			?>
+		  <div class="wrapper_cards">
+		  	<div>
+			  	<div class="cards_size"><p><?php echo $key; ?></p></div>
+			  	<div class="cards_number"><p><?php echo $sleeve; ?></p></div>
+			  </div>
+			</div>
+		  <?php
+		}
+	}
+}
+?>
