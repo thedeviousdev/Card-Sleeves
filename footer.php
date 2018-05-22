ec124d91b6c23cd080376d7502fac38a0aa67e85
thedeviousdev <blouish@hotmail.com>
2018-05-22 22:52:51 +1000

    Removed live reload

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
-<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+<!-- <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script> -->
   </body>
 </html>
