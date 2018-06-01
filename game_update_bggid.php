8dd6cc85bdf9a9e46a3caefd2dc054af9bc0d5ca
thedeviousdev <blouish@hotmail.com>
2018-06-02 00:18:30 +1000

    Update game BGG IDs

--- game_update_bggid.php
+++ game_update_bggid.php
@@ -1,41 +1,32 @@
 <?php
-$csv_file = "data/games.csv";
-$csv = array_map('str_getcsv', file($csv_file));
-array_walk($csv, function(&$a) use ($csv) {
-  $a = array_combine($csv[0], $a);
-});
-array_shift($csv); # remove column header
+// To update games that were added before I saved the BGG ID
 
+try {
+  $db = new PDO('sqlite:data/game-list_test.sqlite');
+  $result = $db->query("SELECT * FROM Game");
 
-foreach($csv as $game) {
-  $id = $game['id'];
-  $name = trim($game['name']);
-  $year = $game['yearpublished'];
-  $min_player = $game['minplayers'];
-  $max_player = $game['maxplayers'];
-  $url = "https://boardgamegeek.com/boardgame/" . $id;
-  $image = $game['image'];
+  foreach($result as $row) {
+    $url = $row['URL'];
+    $id = $row['Id'];
+    $name = $row['Name'];
+    if($row['URL'] != "") {
+      $parse = parse_url($url);
+      $parts = explode('/', $parse['path']);
+      $bggid = $parts[2];
+        echo 'Name:' . $name . ' BGGID: ' . $bggid .' url: ' . $url . '</br>';
 
-	$extension = pathinfo($image, PATHINFO_EXTENSION);
-	if($extension == NULL)
-		$extension = 'jpg';
+      try {
+        $db->exec("UPDATE Game SET BGGID = '" . $bggid  . "' WHERE Id = '" . $id . "';");
+      }
+      catch(PDOException $e)  {
+        print 'Exception : '. $e->getMessage();
+      }
+    }
+  }
 
-	$clean_name = strtolower(preg_replace("/[^a-zA-Z0-9]+/", "", $name));
-
-	$path = $clean_name . "." . $extension;
-
-  try {
-		$db = new PDO('sqlite:data/game-list_test.sqlite');
-
-  	// $db->exec("UPDATE Game SET URL = '" . $url  . "', Image = '" . $path . "', Year = '" . $year . "' WHERE Name = '" . $name . "';");
-  	$db->exec("UPDATE Game SET BGGID = '" . $id  . "' WHERE Name = '" . $name . "';");
-
-  	// echo $name . ': <br />Year: ' . $year . '<br />Image: ' . $path . '<br />URL: ' . $url . '<br />';
-  	echo $name . ': ' . $id;
-	}
-	catch(PDOException $e) 	{
-	  print 'Exception : '. $e->getMessage();
-	}
+  $db = NULL;
 }
-
+catch(PDOException $e)  {
+  print 'Exception : '. $e->getMessage();
+}
 ?>
